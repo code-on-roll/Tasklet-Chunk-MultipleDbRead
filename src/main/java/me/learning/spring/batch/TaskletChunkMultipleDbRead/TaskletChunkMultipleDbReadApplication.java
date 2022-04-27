1883d55775478f577d30794557e9f22e21022b46
rohan <mandal878@gmail.com>
2022-04-28 02:37:13 +0530

    Project base

---
+++ src/main/java/me/learning/spring/batch/TaskletChunkMultipleDbRead/TaskletChunkMultipleDbReadApplication.java
@@ -0,0 +1,13 @@
+package me.learning.spring.batch.TaskletChunkMultipleDbRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskletChunkMultipleDbReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskletChunkMultipleDbReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/learning/spring/batch/TaskletChunkMultipleDbRead/TaskletChunkMultipleDbReadApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learning.spring.batch.TaskletChunkMultipleDbRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskletChunkMultipleDbReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
